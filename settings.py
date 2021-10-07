d38e6837bf578e473336baea7ce40eaf5775b25f
Gary Desktop <frublob46@gmail.com>
2021-10-07 18:19:02 +0100

    Moving Aliens!

--- settings.py
+++ settings.py
@@ -12,6 +12,12 @@ class Settings:
         # Ship settings
         self.ship_speed = 1.5
 
+        # Alien settings
+        self.alien_speed = 1
+        self.fleet_drop_speed = 10
+        # fleet_direct of 1 represents right; -1 represts left
+        self.fleet_direction = 1
+
         # Bullet settings
         self.bullet_speed = 1.0
         self.bullet_width = 3
